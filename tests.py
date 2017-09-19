eadf4d5b94e38d1b220eded3aa25a0ae4d370f1e
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-09-19 21:26:56 +0200

    fix tests

--- tests.py
+++ tests.py
@@ -46,5 +46,5 @@ def test_origin_switch():
 
 
 def test_graph_center():
-    assert graph.graph_center(((2, 1), (1, 2), (2, 3), (3, 2))) == (2., 2.)
-    assert graph.graph_center(((2, 1, 0), (1, 2, 0), (2, 3, 0), (3, 2, 0))) == (2., 2., 0)
+    assert graph.center(((2, 1), (1, 2), (2, 3), (3, 2))) == (2., 2.)
+    assert graph.center(((2, 1, 0), (1, 2, 0), (2, 3, 0), (3, 2, 0))) == (2., 2., 0)
