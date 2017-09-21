2ccb097c2beb3ffb394d085913f7f179dc797ab8
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-09-21 18:51:08 +0200

    geometry,tests: implements generalistic geometry, including rotation of coordinates system

---
+++ geometry.py
@@ -0,0 +1,83 @@
+"""Definition of geometry.
+
+"""
+import math
+from collections import namedtuple
+
+Coords = namedtuple('Coords', 'x, y, z')
+Coords2D = namedtuple('Coords2D', 'x, y')
+
+
+def coords_in_system(point:Coords, system_origin:Coords,
+                     system_rotation:Coords) -> Coords:
+    """Return the coords of given point in the coordinate system
+    of given origin and with given angles (in degrees) with the standard system.
+
+    """
+    rotated = with_rotated_axis(point, system_rotation)
+    return coords_centered_on(rotated, system_origin)
+
+
+def with_rotated_axis(point:Coords, rotation:Coords) -> Coords:
+    """Returns coordinates of given point after given rotation
+    (in degrees) for each axis"""
+    x_rotation, y_rotation, z_rotation = map(math.radians, rotation)
+    px, py, pz = point
+    cos, sin = math.cos, math.sin
+    if x_rotation:
+        py, pz = (
+            py * cos(x_rotation) - pz * sin(x_rotation),
+            py * sin(x_rotation) + pz * cos(x_rotation)
+        )
+    if y_rotation:
+        px, pz = (
+            px * cos(y_rotation) - pz * sin(y_rotation),
+            px * sin(y_rotation) + pz * cos(y_rotation)
+        )
+    if z_rotation:
+        px, py = (
+            px * cos(z_rotation) - py * sin(z_rotation),
+            px * sin(z_rotation) + py * cos(z_rotation)
+        )
+    return Coords(px, py, pz)
+
+
+def angles_from_coords(coords:Coords) -> Coords:
+    """Return the angles (in degrees) formed by each axis and the line ((0, 0, 0), coords)"""
+    dist = math.sqrt(coords.x**2 + coords.y**2 + coords.z**2)
+    if dist == 0.:  return 0, 0, 0
+    x = math.degrees(math.acos(coords.x / dist))
+    y = math.degrees(math.acos(coords.y / dist))
+    z = math.degrees(math.acos(coords.z / dist))
+    return x, y, z
+
+
+def coords_from_angles(angles:Coords, distance:float=1.) -> Coords:
+    """Return the coords at given distance of origin, and with line ((0, 0, 0), coords)
+    having given angles (in degrees) with axis.
+
+    """
+    if distance == 0.:
+        return Coords(0, 0, 0)
+    # print(*((math.radians(angle)) for angle in angles))
+    # print(*(math.sin(math.radians(angle)) for angle in angles))
+    # print(*(distance * math.cos(math.radians(angle)) for angle in angles))
+    return Coords(*(distance * math.cos(math.radians(angle)) for angle in angles))
+
+
+
+def distance_to_origin(coords) -> float:
+    return distance_between(coords, (0, 0, 0))
+def distance_between(a:Coords, b:Coords) -> float:
+    ax, ay, az = a
+    bx, by, bz = b
+    # print('EYTTEC:', (ax - bx), (ay - by), (az - bz))
+    return math.sqrt((ax - bx)**2 + (ay - by)**2 + (az - bz)**2)
+
+
+def coords_centered_on(obj:Coords, origin:Coords) -> Coords:
+    """Return the coords of object in the coordinate system is centered
+    on given coords.
+
+    """
+    return Coords(x=obj.x - origin.x, y=obj.y - origin.y, z=obj.z - origin.z)
--- tests.py
+++ tests.py
@@ -3,26 +3,36 @@ import projection
 from projection import Coords
 import graph
 import render_gif
+import geometry
+from geometry import Coords
 
 
-def test_angles_with_origin():
+def test_angles_from_coords():
+    angles_from_coords = geometry.angles_from_coords
     for x in range(1, 100, 10):
-        assert projection.angles_with_origin(Coords(x, 0, 0)) == (0, 90, 90)
+        assert angles_from_coords(Coords(x, 0, 0)) == (0, 90, 90)
     for x in range(1, 100, 10):
-        assert projection.angles_with_origin(Coords(0, x, 0)) == (90, 0, 90)
+        assert angles_from_coords(Coords(0, x, 0)) == (90, 0, 90)
     for x in range(1, 100, 10):
-        assert projection.angles_with_origin(Coords(0, 0, x)) == (90, 90, 0)
-    assert tuple(map(round, projection.angles_with_origin(Coords(0, 1, 1)))) == (90, 45, 45)
-    assert tuple(map(round, projection.angles_with_origin(Coords(1, 0, 1)))) == (45, 90, 45)
-    assert tuple(map(round, projection.angles_with_origin(Coords(1, 1, 0)))) == (45, 45, 90)
+        assert angles_from_coords(Coords(0, 0, x)) == (90, 90, 0)
+    assert tuple(map(round, angles_from_coords(Coords(0, 1, 1)))) == (90, 45, 45)
+    assert tuple(map(round, angles_from_coords(Coords(1, 0, 1)))) == (45, 90, 45)
+    assert tuple(map(round, angles_from_coords(Coords(1, 1, 0)))) == (45, 45, 90)
+
+
+def test_coords_from_angles():
+    coords_from_angles = geometry.coords_from_angles
+    assert tuple(map(round, coords_from_angles(Coords(90, 45, 45), math.sqrt(2)))) == (0, 1, 1)
+    assert tuple(map(round, coords_from_angles(Coords(45, 90, 45), math.sqrt(2)))) == (1, 0, 1)
+    assert tuple(map(round, coords_from_angles(Coords(45, 45, 90), math.sqrt(2)))) == (1, 1, 0)
 
 
 def test_distance():
-    dist = projection.distance_between
-    odist = projection.distance_to_origin
+    dist = geometry.distance_between
+    odist = geometry.distance_to_origin
 
     a, b = Coords(0, 0, 0), Coords(1, 1, 1)
-    assert projection.distance_to_origin(a) == 0.
+    assert geometry.distance_to_origin(a) == 0.
     assert dist(a, a) == dist(b, b) == 0.
     assert dist(a, b) == odist(b) == math.sqrt(3)
 
@@ -36,15 +46,35 @@ def test_distance():
 
 
 def test_origin_switch():
-    dist = projection.distance_between
+    dist = geometry.distance_between
     a, b = Coords(0, 0, 0), Coords(1, 1, 1)
     c, d, e = Coords(1, 0, 0), Coords(0, 1, 0), Coords(0, 0, 1)
-    assert projection.coords_centered_on(a, b) == Coords(-1, -1, -1)
-    assert projection.coords_centered_on(c, a) == c
-    assert projection.coords_centered_on(d, a) == d
-    assert projection.coords_centered_on(e, a) == e
+    assert geometry.coords_centered_on(a, b) == Coords(-1, -1, -1)
+    assert geometry.coords_centered_on(c, a) == c
+    assert geometry.coords_centered_on(d, a) == d
+    assert geometry.coords_centered_on(e, a) == e
 
 
 def test_graph_center():
     assert graph.center(((2, 1), (1, 2), (2, 3), (3, 2))) == (2., 2.)
     assert graph.center(((2, 1, 0), (1, 2, 0), (2, 3, 0), (3, 2, 0))) == (2., 2., 0)
+
+
+def test_coords_in_system():
+    ROUNDING = 4
+    rounded = lambda c: Coords(round(c.x, ROUNDING), round(c.y, ROUNDING), round(c.z, ROUNDING))
+    coords_in_system = lambda a,b,c: rounded(geometry.coords_in_system(a,b,c))
+    a, null = Coords(1, 1, 0), Coords(0, 0, 0)
+    assert coords_in_system(a, a, null) == null
+    assert coords_in_system(a, null, null) == a
+    assert coords_in_system(null, null, null) == null
+    assert coords_in_system(null, a, null) == Coords(-1, -1, 0)
+
+    # now, work with angle
+    assert rounded(coords_in_system(Coords(1, 1, 0), null, Coords(0, 0, -45))) == rounded(Coords(math.sqrt(2), 0, 0))
+    assert rounded(coords_in_system(Coords(1, 1, 0), null, Coords(0, 0, -90))) == rounded(Coords(1, -1, 0))
+
+    # now, rotation and translation !
+    for x in range(-90, 90, 20):
+        assert rounded(coords_in_system(null, a, Coords(0, 0, x))) == rounded(Coords(-1, -1, 0))
+    assert rounded(coords_in_system(a, null, Coords(0, -90, 0))) == rounded(Coords(0, 1, -1))
